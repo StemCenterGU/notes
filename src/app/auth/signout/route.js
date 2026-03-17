f58f81893ad58b7afa9beff68bdb59492ea6cf3e
Avish Tejas Maniar <avishmaniar24@gmail.com>
2026-03-17 15:01:47 -0400

    feat: Add API route for user sign-out functionality.

--- src/app/auth/signout/route.js
+++ src/app/auth/signout/route.js
@@ -3,10 +3,16 @@ import { revalidatePath } from 'next/cache'
 import { NextResponse } from 'next/server'
 
 export async function POST(request) {
+  const baseUrl = process.env.NEXT_PUBLIC_APP_URL
+
+  if (!baseUrl) {
+    throw new Error('NEXT_PUBLIC_APP_URL is not defined')
+  }
+
   const supabase = await createClient()
   await supabase.auth.signOut()
   revalidatePath('/', 'layout')
-  return NextResponse.redirect(new URL('/', request.url), {
+  return NextResponse.redirect(`${baseUrl}/`, {
     status: 302,
   })
 }
